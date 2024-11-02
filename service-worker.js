71540793f45f1ece8693a6c3557843e914587580
Raissichamseddine <chams198711@gmail.com>
2024-11-02 02:11:31 +0100

    Update service-worker.js
    Signed-off-by: Raissichamseddine <chams198711@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,15 @@
-const CACHE_NAME = 'besafe-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log(`Deleting old cache: ${cacheName}`);
+            return caches.delete(cacheName);
           }
         })
       );
     })
   );
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('push', (event) => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.message,
-    icon: '/icons/icon-192x192.png'
-  });
-});
-npm install -g http-server
-cd chemin/vers/le/dossier/be-safe-With-Me
 
