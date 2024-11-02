def7462c505698c571e38dc49cc21244b6a14061
Raissichamseddine <chams198711@gmail.com>
2024-11-02 02:18:43 +0100

    service-worker.js
    Signed-off-by: Raissichamseddine <chams198711@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,62 @@
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log(`Deleting old cache: ${cacheName}`);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+const CACHE_NAME = 'besafe-cache-v1';
+const urlsToCache = [
+'/',
+'/index.html',
+'/styles.css',
+'/icons/icon-192x192.png',
+'/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then((cache) => cache.addAll(urlsToCache))
+);
 });
 
+self.addEventListener('activate', (event) => {
+event.waitUntil(
+caches.keys().then((cacheNames) => {
+return Promise.all(
+cacheNames.map((cache) => {
+if (cache !== CACHE_NAME) {
+return caches.delete(cache);
+}
+})
+);
+})
+);
+});
+
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+caches.match(event.request)
+.then((response) => {
+return response || fetch(event.request);
+})
+);
+});
+
+self.addEventListener('push', (event) => {
+const data = event.data.json();
+self.registration.showNotification(data.title, {
+body: data.message,
+icon: '/icons/icon-192x192.png'
+});
+});
+npm install -g http-server
+cd chemin/vers/le/dossier/be-safe-With-Me
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          // Optionnel : gérer les erreurs réseau (par exemple, si l'utilisateur est hors ligne)
+        });
+      })
+  );
+});
+http-server -o
